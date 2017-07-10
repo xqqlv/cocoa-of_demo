bc8a6b5c018c20f0b0a6bc6655c8576824317260
“v_xuqiangqiang” <v_xuqiangqiang@dds.com>
2017-07-10 10:33:58 +0800

    第一次工程提交

---
+++ CocoaPodDemo/CocoaPodDemo/CocoaPodDemo.h
@@ -0,0 +1,15 @@
+//
+//  CocoaPodDemo.h
+//  CocoaPodDemo
+//
+//  Created by 徐强强 on 2017/7/10.
+//  Copyright © 2017年 徐强强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodDemo : NSObject
+
++ (void)test;
+
+@end
---
+++ CocoaPodDemo/CocoaPodDemo/CocoaPodDemo.m
@@ -0,0 +1,17 @@
+//
+//  CocoaPodDemo.m
+//  CocoaPodDemo
+//
+//  Created by 徐强强 on 2017/7/10.
+//  Copyright © 2017年 徐强强. All rights reserved.
+//
+
+#import "CocoaPodDemo.h"
+
+@implementation CocoaPodDemo
+
++ (void)test {
+    NSLog(@"test");
+}
+
+@end
